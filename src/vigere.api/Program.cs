1e63aeb5bea2759e4cd82fad15ba41ac4b78b919
Felipe Jonathan <felipejonathan.dev@gmail.com>
2025-12-17 18:11:37 -0300

    config: adicionando a biblioteca scalar para visualização e documentação das rotas de API

--- src/vigere.api/Program.cs
+++ src/vigere.api/Program.cs
@@ -1,23 +1,21 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
