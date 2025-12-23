6abcd2c8fb9a964e494c453982a743d75990327e
Felipe Jonathan <felipejonathan.dev@gmail.com>
2025-12-23 07:17:31 -0300

    feat: adicionando base do pacote de comunicação

---
+++ src/vigere.comunication/Responses/ApiErrorResponseJson.cs
@@ -0,0 +1,9 @@
+﻿namespace vigere.comunication.Responses;
+
+public class ApiErrorResponseJson(string? message, List<string>? codes) 
+{
+    public string? Message { get; } = message;
+    public List<string>? ErrorCodes { get; } = codes;
+
+    public ApiErrorResponseJson(string message) : this(message, ["unknow.error"]) {}
+}
