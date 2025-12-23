bc5cf053fe1134bba3ed42cf3eb503a2594c1e59
Felipe Jonathan <felipejonathan.dev@gmail.com>
2025-12-23 07:46:42 -0300

    feat: adicionando axception handler e padronização das mensagens e códigos de erro

---
+++ src/vigere.api/Handlers/ApiExceptionHandler.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using vigere.exceptions.Resources;
+using vigere.comunication.Responses;
+using vigere.exceptions;
+
+namespace vigere.api.Handlers;
+
+public class ApiExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken
+    )
+    {
+        var (statusCode, message) = exception switch
+        {
+            VigereException ex => HandleDomainException(ex),
+            _ => HandleUnknowException(exception)
+        };
+
+        httpContext.Response.StatusCode = statusCode;
+        httpContext.Response.ContentType = "application/json";
+
+        await httpContext.Response.WriteAsJsonAsync(
+            message,
+            cancellationToken
+        );
+
+        return true;
+    }
+
+    private (int, ApiErrorResponseJson) HandleDomainException(VigereException exception)
+    {
+        return (
+            exception.StatusCode,
+            new ApiErrorResponseJson(
+                exception.Message,
+                exception.GetErrorCodes() ?? []
+            )
+        );
+    }
+
+    private (int, ApiErrorResponseJson) HandleUnknowException(Exception exception)
+    {
+        return exception switch
+        {
+            _ => (
+                StatusCodes.Status500InternalServerError,
+                new ApiErrorResponseJson(ResourceErrorMessages.UNEXPECTED_ERROR)
+            )
+        };
+    }
+}
---
+++ src/vigere.exceptions/VigereException.cs
@@ -0,0 +1,7 @@
+﻿namespace vigere.exceptions;
+
+public abstract class VigereException(string message) : SystemException(message)
+{
+    public abstract int StatusCode { get; }
+    public abstract List<string> GetErrorCodes();
+}
